9bfc0934cc8cdc628aa8e7ca4aa3d44e697a4ece
Peter Kulko <93188219+PKulkoRaccoonGang@users.noreply.github.com>
2022-11-20 23:29:48 +0200

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import axe from '@axe-core/react';
 import App from './App';
 import { store } from './store';
 import './firebase';
 
 import './core/scss/_global.scss';
-
-if (process.env.NODE_ENV !== 'production') {
-	const DEBOUNCE = 1000;
-	axe(React, ReactDOM, DEBOUNCE);
-}
 
 render(
 	<React.StrictMode>
